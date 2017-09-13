5c5c6fa174cc282b16c0fb63b33fad49c193e86c
Shuhel Ahmed <ahmedshuhel@newscred.com>
2017-09-13 22:59:42 +0600

    use command output api

--- rplugin/python/google-error.py
+++ rplugin/python/google-error.py
@@ -9,7 +9,7 @@ class Main(object):
 
     @neovim.function('GoogleError')
     def google_error(self, args):
-        error = self.vim.funcs.execute(':1 messages')
+        error = self.vim.command_output('1 messages')
         query =  { 'q': error}
         webbrowser.open('https://www.google.com/search?{}'.format(urllib.urlencode(query)), new=2)
 
